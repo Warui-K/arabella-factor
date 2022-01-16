79a5dea1fffab94ae837bebc866ba08ecd183c87
Warui-K <warui.kamiri@gmail.com>
2022-01-16 19:53:15 +0300

    Added Scroll Reveal animation library

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+ScrollReveal({
+   // reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+});
+
+
+
+ScrollReveal().reveal('.hero-text, .mvb', { delay: 600, origin: 'right' });
+ScrollReveal().reveal('.mission-statement', { delay: 600 });
+ScrollReveal().reveal('.text-left, .author, .card-img', { delay: 600, origin: 'right' });
+ScrollReveal().reveal('.text-right, .rounded', { delay: 600, origin: 'left' });
+ScrollReveal().reveal('.text-title, .icon, .lead', { delay: 600, origin: 'bottom', interval:200 });
+
+
+
+
+
